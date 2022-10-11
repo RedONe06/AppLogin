2cdc68d32d7d73257c62ca7c85a66ddeb1363b06
RedONe06 <andrielejorasdossantos@gmail.com>
2022-10-10 22:07:50 -0300

    :tada: init

---
+++ app/src/main/java/br/ulbra/applogin/MainActivity.java
@@ -0,0 +1,38 @@
+package br.ulbra.applogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btEntrar, btRegistar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btEntrar = (Button) findViewById(R.id.btnEntrar);
+        btRegistar = (Button) findViewById(R.id.btnRegistrar);
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        btRegistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RegistrarActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
